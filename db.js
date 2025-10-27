9edf991eb0c1bec6074d297af629f2ddd30e4fc9
adewoye-saheed-dML <adewoyesaheed001@gmail.com>
2025-10-26 23:20:40 -0700

    stage2

--- db.js
+++ db.js
@@ -1,24 +1,18 @@
 // db.js
-
 const mysql = require('mysql2/promise');
-const fs = require('fs');
 require('dotenv').config();
 
-// Create the connection pool
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  
-  port: process.env.PORT, 
-
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  maxIdle: 10,
   ssl: {
-    ca: fs.readFileSync('./certs/ca.pem')
-  },
+    ca: process.env.DB_CA.replace(/\\n/g, '\n')
+  }
 });
 
 module.exports = db;
